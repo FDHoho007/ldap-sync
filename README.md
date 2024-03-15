83a20b4be8c79bde18447ac2bc25e5a61858c19b
FDHoho007 <mail@fdhoho007.de>
2024-03-16 00:59:22 +0100

    Replace old code

--- README.md
+++ README.md
@@ -1,18 +1,106 @@
-Services to sync to
-* GitLab
-* Redmine-AK-Bereich (can change uid, enforce)
+# LDAP Sync
 
-* Mailman2 (can change uid)
-* GitHub (can change uid, enforce)
-* Mattermost
-* Matrix (can change uid)
+**Required apt packages**: python3-mailmanclient curl
 
+This tool is inteded to sync group memberships from a ldap server to serveral different external providers.
+To use this tool, you first need to decide which providers you want to synchronize to.
+These need to be defined in the `app.py` in the `providers` array and given their section of the config.
+Next you need to create and fill out the app.conf. The different configuration options will be explained later or in the respective provider section.
+All thats left is to define mappings for each provider in `mappings/<provider>.json`. The mapping scheme is defined in the respective provider section. A mapping file always consists of an array of mappings. It is generally advised to define higher privileged mappings before others for the same provider group. Example given: First define the admin mapping and then the user mapping for a certain GitHub organization.
 
-* Special Attrs
+## General configuration options
 
-Bei Admins treffen ansprechen:
-* GitLab Gruppen dürfen keinem Fachschaftler gehören
-* GitLab Usernames sollten sich nicht ändern lassen
-* Mailman: Wollen wir Benachrichtigungen für uns/mitglieder bei subscription?
-    Es ist davon auszugehen, dass das Script ordnungsgemäß funktioniert und somit genau die Gruppen ausm LDAP abbildet
-    Eine Benachrichtigung sollte also nicht benötigt werden.
+The `ldap` section contains the `uri`, the `bind_dn` and the `bind_pw` used to connect to the ldap server.
+The `member` and `group` section contain information about the ldap scheme of the server. 
+They both have a `base` and `filter` attribute used to query for groups and users.
+They also both have a `uid_attr` which defines the attribute holding the "name" of the group or user.
+Additionally the `group` section conatins the `owner_attr` which holds one or more group admins and the `member_attr` which holds the members of this group (including their owners).
+
+## GitHub Provider
+
+The GitHub provider can be used to sync users to GitHub organizations with either member or admin role.
+
+**Configuration Options**:  
+In the `github` configuration section you need to provide an `api_token` that has write access to the members of your GitHub organizations.
+It is recommended to create a new GitHub account for this to not bind this token to an active user. You do need to provide a `bot_user_login`.
+This username will be ignored during sync and should therefore be the username or your new account.
+The GitHub username of a user will be read out of a messenger attribute. A user can have multiple messenger attributes with their value being the messenger name followed by a colon and the actual username.
+Example given: messenger: "GitHub:FDHoho007" The messenger must be spelled exactly `GitHub`. The name of this messenger attribute is defined in the `messenger_attr`.
+
+**Mapping Scheme**:
+The `mappings/github.json` must contain an array of mappings. A mapping is a simple object holding these four attributes 
+* `id`: The id of the GitHub organization.
+* `name`: A display name for this mapping, shown in the logs.
+* `role`: Either `member` or `admin` defining the user status for the mapped users.
+* `ldap_groups`: An array of ldap groups which should be mapped to this GitHub organization.
+
+## GitLab Provider
+
+The GitLab provider can be used to sync users to GitLab groups with different permission levels.
+
+**Configuration Options**:  
+Since GitLab can be self-hosted you need to provide the `url` of your GitLab instance as well as an `api_token` with write access to your GitLab groups. Finally you need to define the ldap attribute defining the GitLab username of a user in `username_attr`.
+
+**Mapping Scheme**:  
+The `mappings/gitlab.json` must contain an array of mappings. A mapping is a simple object holding these four attributes:
+* `id`: The id of either the project or group. Not the numerical id but the url part containing letters and slashes.
+* `is_project`: Either `true` or `false` defining wether id is a project or a group.
+* `access_level`: The access level for the mapped users. See https://docs.gitlab.com/ee/api/access_requests.html#valid-access-levels
+* `ldap_groups`: An array of ldap groups which should be mapped to this GitLab project or group.
+
+## Mailman3 Provider
+
+The Mailman3 provider can be used to sync users to mailing lists.
+
+**Configuration Options**:  
+This provider needs access tothe internal admin rest api. The address needs to be set in `url` field. The url has the scheme `http://<hostname>:<port>/<api_version>`. You also need to set the `admin_user` and `admin_password`. These information can be found in `/etc/mailman3/mailman.cfg` under the `webservice` section. Mailman also needs the email address and full name of users. The attribute names for these values are defined in `mail_attr` and `name_attr`.
+
+**Mapping Scheme**:  
+The `mappings/mailman3.json` must contain an array of mappings. A mapping is a simple object holding these two attributes:
+* `id`: The id or address of the mailing list.
+* `ldap_groups`: An array of ldap groups which should be mapped to this mailing list.
+
+## Mattermost Provider
+
+The Mattermost provider can be used to sync users to Mattermost teams.
+
+**Configuration Options**:  
+For this provider you need to create a bot account for your mattermost instance (`url` field) who is team administrator in the teams to manage. Set the `api_token` and `bot_user_id` as well as the `username_attr` which contains the Mattermost username of a user.
+
+**Mapping Scheme**:  
+The `mappings/mattermost.json` must contain an array of mappings. A mapping is a simple object holding these four attributes:
+* `id`: The id of the mattermost team.
+* `name`: A display name for this mapping, shown in the logs.
+* `roles`: Either `team_user` or `team_user team_admin` defining the roles of mapped users.
+* `ldap_groups`: An array of ldap groups which should be mapped to this Mattermost team.
+
+## Redmine Provider
+
+The Redmine provider is very specific. It's designed to update a single redmine page with group memberships from the LDAP.
+
+**Configuration Options**:  
+You need to provide the `url` of the page in your redmine instance as well as an `api_token` with write access to this page. The full names of users will be read from the `name_attr`.
+
+**Mapping Scheme**:  
+The `mappings/redmine.json` must contain an array of mappings. A mapping is a simple object holding these two attributes:
+* `id`: The name of the redmine entry.
+* `ldap_groups`: An array of ldap groups which should be mapped to this redmine entry.
+
+## StudIP Provider
+
+The StudIP provider can be used to sync users to a StudIP institution.
+
+**Configuration Options**:  
+You need to provide the `url` of your StudIP instance as wells as the credentials of an admin account for your institution in `username` and `password`. The StudIP username will be read from `username_attr`.
+
+**Mapping Scheme**:  
+The `mappings/studip.json` must contain an array of mappings. A mapping is a simple object holding these four attributes:
+* `id`: The id of your StudIP institution.
+* `role_id`: The id of the member role/group.
+* `name`: A display name for this mapping, shown in the logs.
+* `ldap_groups`: An array of ldap groups which should be mapped to this StudIP institution.
+
+## Possible future providers
+
+* Mattermost channel mappings
+* StudIP Dozentenrechte
--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 #!/bin/python3
-import configparser, argparse, ldap
-from lib.user import User
-from lib.mapper import Mapper
+import configparser, argparse, ldap, time
+from lib import ISetProvider, IUpdateProvider
+from providers.github import GitHubProvider
+from providers.gitlab import GitLabProvider
+from providers.mailman3 import Mailman3Provider
+from providers.mattermost import MattermostProvider
+from providers.redmine import RedmineProvider
+from providers.studip import StudIPProvider
 
 parser = argparse.ArgumentParser(description="Sync (primarly) user groups between ldap and other services.")
 parser.add_argument("--verbose", "-v", help="Print debug information about what the script is doing. (-v only changes, -vv everything)", action="count", default=0)
 parser.add_argument("--dry-run", "-d", help="Do not actaully sync anything. Just pretend to. Useful with --verbose", action="count", default=0)
 parser.add_argument("--config", "-c", help="The location of the config file", default="app.conf")
+parser.add_argument("--loop", "-l", help="Run sync tool continuosly till terminated and wait x seconds in between", default=0)
 args = parser.parse_args()
-DRY_RUN = args.dry_run
+verbose_level = args.verbose
+DRY_RUN = args.dry_run > 0
+def debug(provider, message):
+    if verbose_level >= 2:
+        print("[DEBUG | " + provider + "] " + message)
+def info(provider, message):
+    if verbose_level >= 1:
+        print("[INFO | " + provider + "] " + message)
+def error(provider, message):
+    print("[ERROR | " + provider + "] " + message)
 
 config = configparser.ConfigParser()
 config.read(args.config)
@@ -16,44 +31,97 @@ config.read(args.config)
 ldap_connection = ldap.initialize(config["ldap"]["uri"])
 ldap_connection.simple_bind_s(who=config["ldap"]["bind_dn"], cred=config["ldap"]["bind_pw"])
 
-# Put all active services into this array
-services = []
+# Put all active providers into this array
+providers = [GitHubProvider(config["github"]), GitLabProvider(config["gitlab"]), Mailman3Provider(config["mailman3"]), MattermostProvider(config["mattermost"]), RedmineProvider(config["redmine"]), StudIPProvider(config["studip"])]
 
-def get_attr(user_data, attr):
-    return user_data[config["user"][attr + "_attr"]][0].decode()
+attrs = [config["member"]["uid_attr"]]
+for provider in providers:
+    attrs.extend(provider.attrs)
+attrs = set(attrs)
 
-def get_group(group_dn):
-    if not group_dn.endswith(config["group"]["base"]):
+def sync():
+    ldap_members = {}
+    ldap_groups = {}
+    def getLDAPMember(ldap_member):
+        if ldap_member in ldap_members:
+            return ldap_members[ldap_member]
         return None
-    group_dn = group_dn[0:len(group_dn) - (len(config["group"]["base"]) + 1)]
-    if not group_dn.startswith(config["group"]["uid_attr"] + "="):
-        return None
-    group_dn = group_dn[len(config["group"]["uid_attr"]) + 1:]
-    if "=" in group_dn or "," in group_dn:
-        return None    
-    return group_dn
+    def getLDAPGroupMembers(ldap_group):
+        if ldap_group in ldap_groups:
+            return ldap_groups[ldap_group]
+        return {"owners": [], "members": []}
 
-attrs = []
-for attr in ["uid", "first_name", "last_name", "display_name", "email", "memberof"]:
-    attrs.append(config["user"][attr + "_attr"])
+    for member_data in ldap_connection.search_s(config["member"]["base"], ldap.SCOPE_SUBTREE, config["member"]["filter"], attrs):
+        member_data = member_data[1]
+        member = {}
+        for attr in attrs:
+            if attr in member_data:
+                value = member_data[attr]
+                member[attr] = value[0].decode() if len(value) == 1 else [x.decode() for x in value]
+        ldap_members[member[config["member"]["uid_attr"]]] = member
+    for group_data in ldap_connection.search_s(config["group"]["base"], ldap.SCOPE_SUBTREE, config["group"]["filter"], [config["group"]["uid_attr"], config["group"]["owner_attr"], config["group"]["member_attr"]]):
+        group = group_data[1][config["group"]["uid_attr"]][0].decode()
+        ldap_groups[group] = {"owners": [], "members": []}
+        for index in ["owner", "member"]:
+            if config["group"][index + "_attr"] in group_data[1]:
+                for member in group_data[1][config["group"][index + "_attr"]]:
+                    member = getLDAPMember(member.decode().split(",")[0].split("=")[1]) # This is probably some guessing. We try to extract the uid from the dn
+                    if member is not None:
+                        ldap_groups[group][index + "s"].append(member)
 
-# Read users from ldap into users array
-users = []
-for user_data in ldap_connection.search_s(config["user"]["base"], ldap.SCOPE_SUBTREE, config["user"]["filter"], attrs):
-    user_data = user_data[1]
-    # Parse user groups
-    groups = []
-    if config["user"]["memberof_attr"] in user_data:
-        for group in user_data[config["user"]["memberof_attr"]]:
-            group = get_group(group.decode())
-            if group is not None:
-                groups.append(group)
-    users.append(User(uid=get_attr(user_data, "uid"),
-                first_name=get_attr(user_data, "first_name"),
-                last_name=get_attr(user_data, "last_name"),
-                display_name=get_attr(user_data, "display_name"),
-                email=get_attr(user_data, "email"),
-                groups=groups))
+    for provider in providers:
+        provider_name = provider.name
+        ldap_members = {}
+        names = {}
+        processed_groups = []
+        for group in provider.getGroups():
+            group_name = group["name"]
+            current_members = group["members"] # member ids for provider
+            mapped_members = [] # member ids for provider
+            group["owners"] = []
+            for ldap_group in group["ldap_groups"]:
+                if not ldap_group in ldap_members:
+                    members = getLDAPGroupMembers(ldap_group)
+                    ldap_members[ldap_group] = {}
+                    for index in ["owners", "members"]:
+                        ldap_members[ldap_group][index] = []
+                        for ldap_member in members[index]:
+                            member_id = provider.getMemberId(ldap_member)
+                            if member_id is not None:
+                                ldap_members[ldap_group][index].append(member_id)
+                                names[member_id] = ldap_member[config["member"]["uid_attr"]]
+                mapped_members.extend(ldap_members[ldap_group]["members"])
+                group["owners"].extend(ldap_members[ldap_group]["owners"])
+            group["mapped_members"] = mapped_members
+            current_members = set(current_members)
+            mapped_members = set(mapped_members)
+            processed_members = set(provider.getProcessedMembers(processed_groups, group))
+            if isinstance(provider, ISetProvider):
+                if current_members == mapped_members:
+                    debug(provider_name, "Group " + group_name + " is already up-to-date.")
+                else:
+                    if not DRY_RUN:
+                        provider.setMembers(group, list(mapped_members))
+                    info(provider_name, "Synced all members of group " + group_name + ".")
+            elif isinstance(provider, IUpdateProvider):
+                updated = False
+                for member in current_members - mapped_members - processed_members:
+                    if not DRY_RUN:
+                        provider.removeMember(group, member)
+                    if provider_name != "GitHub": # We dont remove GitHub users, dont spam logs.
+                        info(provider_name, "Removed member " + str(member) + " from group " + group_name + ".")
+                    updated = True
+                for member in mapped_members - current_members - processed_members:
+                    if not DRY_RUN:
+                        provider.addMember(group, member)
+                    info(provider_name, "Added member " + names[member] + "(" + str(member) + ") to group " + group_name + ".")
+                    updated = True
+                if not updated:
+                    debug(provider_name, "Group " + group_name + " is already up-to-date.")
+            processed_groups.append(group)
 
-for service in services:
-    service.synchronize_all(users)
+sync()
+while args.loop > 0:
+    debug("Sleep", "Sleeping for " + str(args.loop) + " seconds.")
+    time.sleep(args.loop)
+    sync()
